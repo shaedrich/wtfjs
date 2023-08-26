9907bee2d5353a0da9df67e3af364f487a516529
Sebastian Hädrich <sebastian.haedrich@mailbox.org>
2023-08-26 23:43:21 +0200

    Replace inactive maintainer @brianleroux with new maintainer @shaedrich

--- app/elements/site-layout.mjs
+++ app/elements/site-layout.mjs
@@ -14,12 +14,14 @@ export default function layout({ html }) {
                 <a href="https://twitter.com/alunny">Andrew Lunny</a>.
                 sparodically uncurated by
                 <a href="http://dtrejo.com">David Trejo</a>.
+                maintained by
+                <a href="https://sebastian-haedrich.de/webdev">Sebastian Hädrich</a>.
             </h2>
 
             <div id="content"><slot></slot></div>
-            <div id="foot">wtfjs is <a href="/license">free software</a>. get the <a href="http://github.com/brianleroux/wtfjs">source on github</a>. </div>
+            <div id="foot">wtfjs is <a href="/license">free software</a>. get the <a href="http://github.com/shaedrich/wtfjs">source on github</a>. </div>
         </div>
-        <a href="http://github.com/brianleroux/wtfjs"><img style="position: absolute; top: 0; right: 0; border: 0;" src="/_public/forkme_right_gray_6d6d6d.svg" alt="Fork me on GitHub" /></a>
+        <a href="http://github.com/shaedrich/wtfjs"><img style="position: absolute; top: 0; right: 0; border: 0;" src="/_public/forkme_right_gray_6d6d6d.svg" alt="Fork me on GitHub" /></a>
         <script src="/jquery-1.4.2.min.js" type="text/javascript" charset="utf-8"></script>
     </body>
 </html>
